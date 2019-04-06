732681e2fe0bf74c7101d23624311b73a78673f8
safa <1439325342@qq.com>
2019-04-06 20:05:38 +0800

    更新 demo

---
+++ demo/dist/precache-manifest.211cdecf22cc940bd66a370e8113bee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "61a20ad9e4dd83296689",
+    "url": "js/chunk-vendors.4ae9f449.js"
+  },
+  {
+    "revision": "9559dd7df5def706b80d",
+    "url": "js/app.8ef75234.js"
+  },
+  {
+    "revision": "06a073e54e351f278a06092686efdbfe",
+    "url": "index.html"
+  },
+  {
+    "revision": "61a20ad9e4dd83296689",
+    "url": "css/chunk-vendors.814d6bc3.css"
+  },
+  {
+    "revision": "9559dd7df5def706b80d",
+    "url": "css/app.573d4e74.css"
+  }
+];
